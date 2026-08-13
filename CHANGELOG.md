4c400d0c5c27d37f3938aee14617a171c9abe65c
joabssilveira <joabssilveira@gmail.com>
2026-08-13 13:05:31 -0300

    feat: first commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,4 +4,4 @@ All notable changes to this project will be documented in this file. See [standa
 
 ### [0.0.2](https://github.com/joabssilveira/fwork-jsts-test/compare/v0.0.1...v0.0.2) (2026-08-13)
 
-### [0.0.1] (2026-08-13)
+### [0.0.1](2026-08-13)
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+export type MyType = {
+
+}
+
+export interface IMyInterface {
+
+}
+
+export class MyClass {
+  
+}
+
+export enum MyEnum {
+
+}
+
+export function myFunc () {
+
+}
+
+export const myArrowFunc = () => {
+
+}
+
+export var myVar = undefined
+
+export const myConst = undefined
